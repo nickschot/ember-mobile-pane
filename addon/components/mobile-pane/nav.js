9c0bd6c62cd2a50445d861b160d583bd19e8b98f
Nick Schot <nickschot@gmail.com>
2018-02-06 23:07:36 +0100

    implement initial prototype of navbar scroll follow

--- addon/components/mobile-pane/nav.js
+++ addon/components/mobile-pane/nav.js
@@ -3,7 +3,7 @@ import layout from '../../templates/components/mobile-pane/nav';
 
 import ComponentParentMixin from 'ember-mobile-pane/mixins/component-parent';
 import NavItem from 'ember-mobile-pane/components/mobile-pane/nav/item';
-import { computed, get, observer } from '@ember/object';
+import { computed, get, set, observer } from '@ember/object';
 
 export default Component.extend(ComponentParentMixin, {
   layout,
@@ -13,9 +13,11 @@ export default Component.extend(ComponentParentMixin, {
 
   onItemClick(){},
 
-  navOffsetChanged: observer('navOffset', 'childNavItems.@each.elementId', 'elementId', function(){
+  navOffsetChanged: observer('navOffset', 'activeIndex', 'childNavItems.@each.elementId', 'elementId', function(){
     const childNavItems = get(this, 'childNavItems');
     const navOffset = get(this, 'navOffset');
+    const activeIndex = get(this, 'activeIndex');
+    const navScrollLeftOffset = 75;
 
     const e1Index = Math.floor(navOffset);
     const e2Index = Math.ceil(navOffset);
@@ -26,24 +28,82 @@ export default Component.extend(ComponentParentMixin, {
     ){
       // the first element is always present
       const e1Dims  = get(childNavItems.objectAt(e1Index), 'element').getBoundingClientRect();
+      const e2Dims = get(childNavItems.objectAt(e2Index), 'element').getBoundingClientRect();
 
       let targetLeft  = e1Dims.left;
       let targetWidth = e1Dims.width;
 
-      if(e1Index !== e2Index){
-        const e2Dims = get(childNavItems.objectAt(e2Index), 'element').getBoundingClientRect();
-        const relativeOffset = navOffset - e1Index;
+      const relativeOffset = navOffset - e1Index;
 
+      if(e1Index !== e2Index){
         // map relativeOffset to correct ranges
         targetLeft  = relativeOffset * (e2Dims.left - e1Dims.left) + e1Dims.left;
         targetWidth = (1 - relativeOffset) * (e1Dims.width - e2Dims.width) + e2Dims.width;
       }
 
       const indicator = document.getElementById(`${get(this, 'elementId')}-nav__indicator`);
+
+      // correct for nav scroll and offset to viewport
+      const parentLeft = get(this, 'element').getBoundingClientRect().left;
+      const parentScrollLeft = get(this, 'element').scrollLeft;
+
+      //indicator.style.width = `${targetWidth}px`;
+      //indicator.style.left  = `${targetLeft - parentLeft + parentScrollLeft}px`;
+
+      // make scroll follow pan and click
+      const targetIsElement1 = navOffset - activeIndex < 0;
+      const targetElementIndex = targetIsElement1 ? e1Index : e2Index;
+
+      if(targetElementIndex === activeIndex){
+        // activeIndex was not changed by panning
+        // change scroll based on target position
+        const targetElementLeft = targetIsElement1 ? e1Dims.left : e2Dims.left;
+        //get(this, 'element').scrollLeft += targetElementLeft - navScrollLeftOffset;
+
+        const targetScrollLeft = get(this, 'element').scrollLeft + (targetElementLeft - navScrollLeftOffset);
+        let currentScrollLeft = get(this, 'element').scrollLeft;
+        //set(this, 'startTS', performance.now());
+        //this.finishNavScrollTransition();
+
+        $(this.element).animate({scrollLeft: targetScrollLeft}, 200, 'linear');
+
+
+
+        /*const targetElementScrollLeft = targetElementLeft - navScrollLeftOffset;
+        get(this, 'element').getElementsByTagName('ul')[0].style.transform = `translateX(${/!*get(this, 'element').scrollLeft + *!/targetElementScrollLeft}px)`;
+
+        setTimeout(() => {
+          get(this, 'element').getElementsByTagName('ul')[0].style.transform = 'translateX(0)px';
+          get(this, 'element').scrollLeft += targetElementScrollLeft;
+        }, 2000);*/
+      } else {
+        // activeIndex was changed by panning
+        // change scroll based on indicator position
+        const fromLeft = indicator.getBoundingClientRect().left - parentLeft;
+
+        if(fromLeft > 50){
+          get(this, 'element').scrollLeft += fromLeft - navScrollLeftOffset;
+        } else {
+          get(this, 'element').scrollLeft -= navScrollLeftOffset - fromLeft;
+        }
+      }
+
       indicator.style.width = `${targetWidth}px`;
-      indicator.style.left  = `${targetLeft}px`;
+      indicator.style.left  = `${targetLeft - parentLeft + parentScrollLeft}px`;
     }
   }),
+
+  /*duration: 2000,
+  startTS: 0,
+  finishNavScrollTransition(timestamp){
+    //const targetScrollLeft: get(this, 'targetScrolLLeft');
+    const duration = get(this, 'duration');
+    const tsDiff = timestamp - get(this, 'startTS');
+
+    if(currentScrollLeft !== targetScrollLeft){
+      requestAnimationFrame(this.finishNavScrollTransition);
+    }
+  },*/
 
   childNavItems: computed.filter('children', function(view) {
     return view instanceof NavItem;
