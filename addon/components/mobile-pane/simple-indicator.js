92b48070dfde54d8908d071460f7128571277988
Nick Schot <nickschot@gmail.com>
2018-02-14 13:49:45 +0100

    add warp effect

--- addon/components/mobile-pane/simple-indicator.js
+++ addon/components/mobile-pane/simple-indicator.js
@@ -9,6 +9,9 @@ export default Component.extend({
 
   classNames: ['scroller__simple-indicator'],
 
+  // public
+  warpEnabled: false,
+
   // protected
   navItems: null,
   currentOffset: 0,
@@ -16,7 +19,20 @@ export default Component.extend({
   onClick(){},
 
   indicatorStyle: computed('currentOffset', function(){
-    return htmlSafe(`transform: translateX(${-1 * get(this, 'currentOffset') * get(this, 'navItems.length')}%);`);
+    const offset = -1 * get(this, 'currentOffset') * get(this, 'navItems.length');
+    let style = `transform: translateX(${offset}%)`;
+
+    if(get(this, 'warpEnabled')){
+      // warp effect
+      const fraction = offset % 100 / 100;
+      const scale = (2 * fraction - 2 * Math.pow(fraction, 2));
+      const scaleY = 1 - scale / 1.5;
+      const scaleX = 1 + 1.5 * scale;
+      style += ` scale(${scaleX}, ${scaleY})`;
+    }
+    style += ';';
+
+    return htmlSafe(style);
   }),
 
   actions: {
