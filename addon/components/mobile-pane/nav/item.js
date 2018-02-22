8eaa69d2a77d6f906f90921dab6b26d616ae5a93
Nick Schot <nickschot@gmail.com>
2018-02-22 19:36:26 +0100

    fix missing import

--- addon/components/mobile-pane/nav/item.js
+++ addon/components/mobile-pane/nav/item.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import layout from '../../../templates/components/mobile-pane/nav/item';
 
 import ComponentChildMixin from 'ember-mobile-pane/mixins/component-child';
-import { get } from '@ember/object';
+import { get, computed } from '@ember/object';
 
 export default Component.extend(ComponentChildMixin, {
   layout,
