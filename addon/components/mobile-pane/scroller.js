11182a50e5bbad8189b4dfa0177ed5b752096f76
Nick Schot <nickschot@gmail.com>
2019-01-04 15:17:39 +0100

    remove todo

--- addon/components/mobile-pane/scroller.js
+++ addon/components/mobile-pane/scroller.js
@@ -42,7 +42,6 @@ export default Component.extend(RecognizerMixin, {
 
     style += `transform: translateX(${get(this, 'currentOffset')}%)`;
 
-    //TODO: don't use ember binds to set this
     return htmlSafe(style);
   }),
 
