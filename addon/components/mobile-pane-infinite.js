3ede188e247fcc76be5d9fa1d6a031ec51f985b5
Nick Schot <nickschot@gmail.com>
2020-07-02 18:09:05 +0200

    migrate mobile-pane-infinite component to glimmer component

--- addon/components/mobile-pane-infinite.js
+++ addon/components/mobile-pane-infinite.js
@@ -1,21 +1,15 @@
-import Component from '@ember/component';
-import layout from '../templates/components/mobile-pane-infinite';
-
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { get, set, computed, observer } from '@ember/object';
 import { once } from '@ember/runloop';
-import { A } from '@ember/array';
 
 /**
  * @class MobilePaneInfiniteComponent
  */
-export default Component.extend({
-  layout,
-
-  classNames: ['mobile-pane__infinite-scroller'],
-
-  router: service(),
-  memory: service('memory-scroll'),
+export default class MobilePaneInfiniteComponent extends Component {
+  @service router;
+  @service('memory-scroll') memory;
 
   /**
    * Model for the previous pane. Must be truthy to render the pane.
@@ -23,7 +17,6 @@ export default Component.extend({
    * @argument previousModel
    * @default null
    */
-  previousModel: null,
 
   /**
    * Model for the current pane.
@@ -31,7 +24,6 @@ export default Component.extend({
    * @argument currentModel
    * @required
    */
-  currentModel: null,
 
   /**
    * Model for the next pane.
@@ -39,7 +31,6 @@ export default Component.extend({
    * @argument nextModel
    * @default null
    */
-  nextModel: null,
 
   /**
    * Whether or not a router transition is triggered after pane change.
@@ -48,7 +39,6 @@ export default Component.extend({
    * @default false
    * @private
    */
-  transitionAfterDrag: false,
 
   /**
    * Whether or not panning is enabled
@@ -57,7 +47,6 @@ export default Component.extend({
    * @type {boolean}
    * @default false
    */
-  disabled: false,
 
   /**
    * Hook called when the active pane changes.
@@ -66,82 +55,106 @@ export default Component.extend({
    * @param model The model that belongs to the new index
    * @param activeIndex The new index
    */
-  onChange(model, activeIndex){}, //eslint-disable-line no-unused-vars
+
+  /**
+   * Hook called when a drag is started.
+   *
+   * @argument onDragStart
+   */
+
+  /**
+   * Hook called when a drag moved.
+   *
+   * @argument onDragMove
+   * @param dx The delta of the drag
+   */
+
+  /**
+   * Hook called when a drag ended.
+   *
+   * @argument onDragEnd
+   * @param activeIndex The index of the pane on which the drag ended
+   */
 
   //private
-  prevChildScroll: 0,
-  currentChildScroll: 0,
-  nextChildScroll: 0,
-  childOffsetTop: 0,
+  @tracked prevChildScroll = 0;
+  @tracked currentChildScroll = 0;
+  @tracked nextChildScroll = 0;
+  @tracked childOffsetTop = 0;
 
-  onDragStart(){},
-  onDragMove(dx){}, //eslint-disable-line no-unused-vars
-  onDragEnd(activeIndex){}, //eslint-disable-line no-unused-vars
-
-  didInsertElement(){
-    this._super(...arguments);
-
-    //TODO: purge scroll states if we came from a higher level route
-    this.restoreScroll();
-  },
-
-  updateActiveIndex: observer('models.@each.id', function(){
+  @action
+  updateActiveIndex(){
     once(() => {
       // we received new models, restore the scroll
       this.restoreScroll();
     });
-  }),
+  }
 
-  activeIndex: computed('previousModel', function(){
-    return this.previousModel ? 1 : 0;
-  }),
+  get activeIndex(){
+    return this.args.previousModel ? 1 : 0;
+  }
 
-  models: computed('previousModel', 'currentModel', 'nextModel', function(){
-    return A([this.previousModel, this.currentModel, this.nextModel].filter(Boolean));
-  }),
+  get models() {
+    return [this.args.previousModel, this.args.currentModel, this.args.nextModel].filter(Boolean);
+  }
 
-  actions: {
-    onDragStart(){
-      // write scroll offset for prev/next children
-      set(this, 'childOffsetTop', document.scrollingElement.scrollTop || document.documentElement.scrollTop);
+  @action
+  onDragStart(){
+    // write scroll offset for prev/next children
+    this.childOffsetTop = document.scrollingElement.scrollTop || document.documentElement.scrollTop;
 
-      this.onDragStart(...arguments);
-    },
-    onDragMove(){
-      this.onDragMove(...arguments);
-    },
-    onDragEnd(targetIndex){
-      // transition to previous or next model
-      const targetModel = this.models.objectAt(targetIndex);
-      if(targetModel !== this.currentModel){
-        // store the scroll position of currentModel
-        this.storeScroll();
-      }
-
-      this.onDragEnd(targetIndex, targetModel);
-    },
-    onChange(index){
-      this.onChange(this.models.objectAt(index), index);
+    if (this.args.onDragStart) {
+      this.args.onDragStart(...arguments);
     }
-  },
+  }
+
+  @action
+  onDragMove(){
+    if (this.args.onDragMove) {
+      this.args.onDragMove(...arguments);
+    }
+  }
+
+  @action
+  onDragEnd(targetIndex){
+    // transition to previous or next model
+    const targetModel = this.models[targetIndex];
+    if(targetModel !== this.args.currentModel){
+      // store the scroll position of currentModel
+      this.storeScroll();
+    }
+
+    if (this.args.onDragEnd) {
+      this.args.onDragEnd(targetIndex, targetModel);
+    }
+  }
+
+  @action
+  onChange(index){
+    if (this.args.onChange) {
+    this.args.onChange(this.models[index], index);
+    }
+  }
 
   storeScroll(){
-    const key = this._buildMemoryKey(this.get('currentModel.id'));
+    const key = this._buildMemoryKey(this.args.currentModel.id);
     this.memory[key] = document.scrollingElement.scrollTop || document.documentElement.scrollTop;
-  },
+  }
 
+  //TODO: purge scroll states if we came from a higher level route
+  @action
   restoreScroll(){
-    const prevKey     = this._buildMemoryKey(this.get('previousModel.id'));
-    const currentKey  = this._buildMemoryKey(this.get('currentModel.id'));
-    const nextKey     = this._buildMemoryKey(this.get('nextModel.id'));
+    const prevKey     = this._buildMemoryKey(this.args.previousModel.id);
+    const currentKey  = this._buildMemoryKey(this.args.currentModel.id);
+    const nextKey     = this._buildMemoryKey(this.args.nextModel.id);
 
-    set(this, 'prevChildScroll', this.memory[prevKey] || 0);
-    set(this, 'currentChildScroll', this.memory[currentKey] || 0);
-    set(this, 'nextChildScroll', this.memory[nextKey] || 0);
-  },
+    this.prevChildScroll = this.memory[prevKey] || 0;
+    this.currentChildScroll = this.memory[currentKey] || 0;
+    this.nextChildScroll = this.memory[nextKey] || 0;
+  }
 
   // utils
   _buildMemoryKey(id){
-    return `mobile-pane/${this.get('router.currentRouteName')}.${id}`;
+    return `mobile-pane/${this.router.currentRouteName}.${id}`;
   }
-});
+}
